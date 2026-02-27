90864f0703f493158bbd04543ce21c7839e3f1fa
Azamat Akbashev <azamatakbashev2017@gmail.com>
2026-02-27 16:32:17 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://ailab-r

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ailab-result-rapidapi.oss-accelerate.aliyuncs.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
